9419d0915d2fa219e2091c6d4386588498a54128
Paul Bardea <pbardea@gmail.com>
2014-03-12 17:46:13 -0400

    Single player working. Double throws it off, commented out.

--- script.js
+++ script.js
@@ -2,87 +2,137 @@ var canvas = document.getElementById("canvas");
 
 var c = canvas.getContext('2d');
 
+//initialize leap controller
 var controller = new Leap.Controller();
 
 var width = 800;
 var height = 600;
 
+//Set canvas size
 canvas.width = width;
 canvas.height = height;
 
+
+//Set text font
 c.font = "30px Arial";
-c.textAlign = 'center';
-c.textBaseline = 'middle';
+
+//Set countdown variables
+var count = 0;
+var countReady = false;
+
+//Initialize message values
+var robotMove = "";
+var move1 = "";
+var move2 = "";
+var msg = "";
+
+var yResetLimit = 40;
+var yCountLimit = 200;
+var y = yCountLimit - 1; //starting y value (yCountLimit > y > yResetLimit)
+var triggerCount = 4;
+
+//set images
+var rock = "http://www.headfirstlabs.com/books/hfjs/ch03/irock/rock.png"
+var paper = "http://fc02.deviantart.net/fs71/f/2010/148/0/8/loose_leaf_paper_by_dianasurvive.png"
+var scissors = "http://www.boston.com/ae/theater_arts/exhibitionist/scissors.png"
+var fist = "";//add fist image
+
+var mode = ""; //can be: nobody, single, double, multi
+var text = "";
 
 controller.on('frame', function( frame ){
-  c.clearRect(0, 0, width, height);
+  c.clearRect(0, 0, width, height);//clear the rectangle
 
-  var numOfHands = frame.hands.length;
-  var move = "";
-  var move1 = "";
-  var move2 = "";
-  var y = 300;
-  var yLimit = 100;
-  var robotMove = "";
-  var rock = "http://www.headfirstlabs.com/books/hfjs/ch03/irock/rock.png"
-  var paper = "http://fc02.deviantart.net/fs71/f/2010/148/0/8/loose_leaf_paper_by_dianasurvive.png"
-  var scissors = "http://www.boston.com/ae/theater_arts/exhibitionist/scissors.png"
 
+  numOfHands = frame.hands.length;
   if (numOfHands < 1){
-    move = ""
-    robotMove = ""
-  }else if (numOfHands == 1){//single player
-    var numOfFingers = frame.fingers.length;
+    mode = "nobody";
+  }else if(numOfHands == 1){
+    mode = "single";
     y = frame.hands[0].palmPosition[1];
+    var numOfFingers = frame.hands[0].fingers.length; 
 
-    if (y > yLimit){
-      if (numOfFingers <= 1){
-        move = "You: Rock";
-        robotMove = "Robot: Paper";
+    if (y <= yResetLimit){
+      count = 0;
+    }else if(y > yResetLimit && y <= yCountLimit && countReady){
+      count += 1;
+      countReady = false;
+    }else if(y > yCountLimit){
+      countReady = true;
+    }
+
+    if (count === triggerCount){//shoot
+      if (numOfFingers === 0 || numOfFingers === 1){
+        //played rock, response is paper
+        text = "Paper";
       }else if(numOfFingers === 2 || numOfFingers === 3){
-        move = "You: Scissors";
-        robotMove = "Robot: Rock";
+        //played scissors, response is rock
+        text = "Rock";
       }else if(numOfFingers === 4 || numOfFingers === 5){
-        move = "You: Paper";
-        robotMove = "Robot: Scissors";
-      }else{
-        move = ""
-        robotMove = "";
+        //played paper, response is scissors
+        text = "Scissors";
       }
-    }
-  }else if (numOfHands == 2) {//2 player
-    var hand1 = frame.hands[0];
-    var hand2 = frame.hands[1];
-
-    y = hand1.palmPosition[1];
-
-    var numOfFingers1 = hand1.fingers.length;
-    var numOfFingers2 = hand2.fingers.length;
-    move1 = "Loading";
-    move2 = "Loading";
-    if (numOfFingers1 <= 1){
-      move1 = "Rock";
-    }else if(numOfFingers1 === 2 || numOfFingers1 === 3){
-      move1 = "Scissors";
-    }else if(numOfFingers1 === 4 || numOfFingers1 === 5){
-      move1 = "Paper";
+    }else if(count === 0){
+      text = "Ready.";
+    }else if(count < triggerCount){
+      text = triggerCount-count;
     }else{
-      move1 = "Can't detect."
+      text = "Reset Count";
     }
+  // }else if(numOfHands == 2){
+  //   mode = "double";
 
-    if (numOfFingers2 <= 1){
-      move2 = "Rock";
-    }else if(numOfFingers2 === 2 || numOfFingers2 === 3){
-      move2 = "Scissors";
-    }else if(numOfFingers2 === 4 || numOfFingers2 === 5){
-      move2 = "Paper";
-    }else{
-      move2 = "Can't detect."
-    }
+  //   var hand1 = frame.hands[0];
+  //   var hand2 = frame.hands[1];
+
+  //   var numOfFingers1 = hand1.fingers.length;
+  //   var numOfFingers2 = hand2.fingers.length;
+
+  //   y = frame.hands[0].palmPosition[1];
+
+  //   if (y <= yResetLimit){
+  //     count = 0;
+  //   }else if(y > yResetLimit && y <= yCountLimit && countReady){
+  //     count += 1;
+  //     countReady = false;
+  //   }else if(y > yCountLimit){
+  //     countReady = true;
+  //   }
+
+  //   if (count === triggerCount){//shoot
+
+  //     if (numOfFingers1 <= 1){
+  //       move1 = "Rock";
+  //     }else if(numOfFingers1 === 2 || numOfFingers1 === 3){
+  //       move1 = "Scissors";
+  //     }else if(numOfFingers1 === 4 || numOfFingers1 === 5){
+  //       move1 = "Paper";
+  //     }else{
+  //       move1 = "Can't detect."
+  //     }
+
+  //     if (numOfFingers2 <= 1){
+  //       move2 = "Rock";
+  //     }else if(numOfFingers2 === 2 || numOfFingers2 === 3){
+  //       move2 = "Scissors";
+  //     }else if(numOfFingers2 === 4 || numOfFingers2 === 5){
+  //       move2 = "Paper";
+  //     }else{
+  //       move2 = "Can't detect."
+  //     }
+  //     text = move1+' '+move2;
+  //   }else if(count < triggerCount){
+  //     text = triggerCount-count;
+  //   }else{
+  //     text = "Reset count";
+  //   }
   }else{
-    move = "Too many hands"; 
+    mode = "multi";
   }
 
-  c.fillText( move+" "+move1+" "+move2+" "+robotMove, width/2, height/2);
+  c.fillText( text, width/2, height/2);
+
+
 });
 
+controller.connect();
